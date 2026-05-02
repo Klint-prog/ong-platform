ae896c700de6ff61c463c30eb3659ad960699e94
Jhonatas Mendes <jhonatas.mendes@outlook.com>
2026-05-02 18:25:32 -0300

    Bootstrap app with PostgreSQL-backed storage

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,21 @@ import './styles/global.css'
 import './styles/print.css'
 import './styles/financeiro-print.css'
 import './styles/financeiro-overview.css'
+import { hydratePostgresLocalStorage, installPostgresLocalStorage } from './services/postgresLocalStorage.js'
 import './pages/financeiro/orcamentoTipoPatch.js'
 import './pages/financeiro/contaTagsPatch.js'
 import './pages/financeiro/financeiroActionsPatch.js'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-)
+async function bootstrap() {
+  await hydratePostgresLocalStorage()
+  installPostgresLocalStorage()
+
+  createRoot(document.getElementById('root')).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  )
+}
+
+bootstrap()
